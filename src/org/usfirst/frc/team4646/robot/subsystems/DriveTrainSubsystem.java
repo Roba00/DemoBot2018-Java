2db5559abfbea277aa7abcb8bc89c4da7192e408
abbajabalroba <abbajabal.roba@gmail.com>
2018-07-30 20:35:20 -0500

    Final commit.
    Edited spark controller channels to work correctly with robot. Deleted
    incomplete programs related to the flag subsystem since this will be the
    final build. (Switching to C++)

--- src/org/usfirst/frc/team4646/robot/subsystems/DriveTrainSubsystem.java
+++ src/org/usfirst/frc/team4646/robot/subsystems/DriveTrainSubsystem.java
@@ -8,10 +8,10 @@ import edu.wpi.first.wpilibj.SpeedControllerGroup;
 
 public class DriveTrainSubsystem extends Subsystem
 {
-	static Spark leftBigDrive = new Spark(0);
-	static Spark leftMiniDrive = new Spark(1);
-	static Spark rightBigDrive = new Spark(3);
-	static Spark rightMiniDrive = new Spark(4);
+	static Spark leftBigDrive = new Spark(2);
+	static Spark leftMiniDrive = new Spark(3);
+	static Spark rightBigDrive = new Spark(0);
+	static Spark rightMiniDrive = new Spark(1);
 
 	static SpeedControllerGroup leftMotorGroup = new SpeedControllerGroup(leftMiniDrive, leftBigDrive);
 	static SpeedControllerGroup rightMotorGroup = new SpeedControllerGroup(rightMiniDrive, rightBigDrive);
